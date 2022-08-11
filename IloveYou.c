83132f55e84a1f928c2e899119b82a2204194baf
lionelarnab <76247019+lionelarnab@users.noreply.github.com>
2022-08-12 00:58:41 +0530

    Update IloveYou.c

--- IloveYou.c
+++ IloveYou.c
@@ -1,4 +1,4 @@
-/*https://www.theurbanpenguin.com/4184-2/*/
+
 
 #include <stdio.h>
 #include <time.h>
@@ -53,25 +53,3 @@ int main () {
   return 0;
 }
 
-/*
-#include <stdio.h>
-#include <windows.h>
-
-int main()
-{
-  unsigned char b,f;
-
-  for (b=0;b<16;b++)
-  {
-    for (f=0;f<16;f++)
-    {
-        SetConsoleTextAttribute (GetStdHandle(STD_OUTPUT_HANDLE), b<<4 | f);
-        printf ("%.2X", b<<4 | f);
-    }
-    printf ("\n");
-  }
-  SetConsoleTextAttribute (GetStdHandle(STD_OUTPUT_HANDLE), 0x07);
-  printf ("\n");
-  return 0;
-}
-*/
